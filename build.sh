96ec5ed33347a5b455fbe3be1351d1d5797b60f9
Jim Schaad <ietf@augustcellars.com>
2017-04-13 10:06:45 -0700

    Sript update

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ set -ev
 
 nuget restore $SLN
 ls packages
-ls packages/Com.AugustCelalrs.COSE.0.1.0
-ls packages/Com.AugustCelalrs.COSE.0.1.0/lib
-ls packages/Com.AugustCelalrs.COSE.0.1.0/lib/portable-net40+s15+win+wpa81+wp8
+ls packages/Com.AugustCellars.COSE.0.1.0
+ls packages/Com.AugustCellars.COSE.0.1.0/lib
+ls packages/Com.AugustCellars.COSE.0.1.0/lib/portable-net40+s15+win+wpa81+wp8
 
 
 # xbuild $SLN /p:Configuation=Release
