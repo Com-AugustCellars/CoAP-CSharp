1219c53902c48c9d37ba4610c8ca2d52ede8a7ae
Jim Schaad <ietf@augustcellars.com>
2017-04-13 10:04:16 -0700

    Hunt for the file

--- build.sh
+++ build.sh
@@ -2,4 +2,10 @@
 set -ev
 
 nuget restore $SLN
-xbuild $SLN /p:Configuation=Release
+ls packages
+ls packages/Com.AugustCelalrs.COSE.0.1.0
+ls packages/Com.AugustCelalrs.COSE.0.1.0/lib
+ls packages/Com.AugustCelalrs.COSE.0.1.0/lib/portable-net40+s15+win+wpa81+wp8
+
+
+# xbuild $SLN /p:Configuation=Release
