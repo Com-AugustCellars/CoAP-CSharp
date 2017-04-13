9cb253a780ad77a120fb903b19d2b76409bad965
Jim Schaad <ietf@augustcellars.com>
2017-04-13 12:29:48 -0700

    Reorder command line
    Also clean out the debugging for packages.

--- build.sh
+++ build.sh
@@ -2,10 +2,5 @@
 set -ev
 
 nuget restore $SLN
-ls packages
-ls packages/Com.AugustCellars.COSE.0.1.0
-ls packages/Com.AugustCellars.COSE.0.1.0/lib
-ls packages/Com.AugustCellars.COSE.0.1.0/lib/portable-net40+s15+win+wpa81+wp8
 
-
-xbuild $SLN /p:Configuation=Release
+xbuild /p:Configuation=Release $SLN
