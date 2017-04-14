89cb291445c3acd6c445b28fb3aa8cfc92f04f5c
Jim Schaad <ietf@augustcellars.com>
2017-04-14 11:50:53 -0700

    Ok - We now recognize that one of these builds is for NET 20 and thus needs to be different.

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ set -ev
 nuget restore $SLN
 
 xbuild /p:Configuation="Release|Mixed Platforms" $SLN
+
+mono ./testrunner/NUnit.Runners.2.6.4/tools/nunit-console.exe ./CoAP.Test/bin/Release/CoAP.Test.dll
