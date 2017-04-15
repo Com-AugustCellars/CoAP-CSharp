a8e45844771701800e45d9dac07c0d1043542ffd
Jim Schaad <ietf@augustcellars.com>
2017-04-15 08:03:27 -0700

    Chnage the nunit pointer and target the test invocation to framework

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ nuget restore $SLN
 
 xbuild /p:Configuration="Release" $SLN
 
-mono ./testrunner/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe ./CoAP.Test/bin/Release/CoAP.Test.dll
+mono ./testrunner/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe ./CoAP.Test/bin/Release/$TARGET/CoAP.Test.dll
