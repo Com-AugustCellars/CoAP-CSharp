170b6ca5b6deb0f0f08b7570f4f35d8d1f2f7737
Jim Schaad <ietf@augustcellars.com>
2017-04-14 20:36:42 -0700

    Try going back on getting a release build
    Use the correct 45 solution file name

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -ev
 
 nuget restore $SLN
 
-xbuild /p:Configuation="Release|Mixed Platforms" $SLN
+xbuild /p:Configuation="Release" $SLN
 
 mono ./testrunner/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe ./CoAP.Test/bin/Release/CoAP.Test.dll
