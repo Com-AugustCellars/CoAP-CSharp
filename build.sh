36c2502a09161a2bcbe518a17b5e1e8924d12747
Jim Schaad <ietf@augustcellars.com>
2017-04-15 10:48:48 -0700

    Case sensitivity

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -ev
 
 nuget restore $SLN
 
-xbuild /p:Configuration=$Version $SLN
+xbuild /p:Configuration=$VERSION $SLN
 
 mono ./testrunner/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe ./CoAP.Test/bin/$Version/$TARGET/CoAP.Test.dll
