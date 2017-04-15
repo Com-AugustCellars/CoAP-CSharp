eb1d13a50e8c428f744f5182d0bfa7d12b3538b4
Jim Schaad <ietf@augustcellars.com>
2017-04-14 21:08:26 -0700

    Some year I will learn to spell.

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -ev
 
 nuget restore $SLN
 
-xbuild /p:Configuation="Release" $SLN
+xbuild /p:Configuration="Release" $SLN
 
 mono ./testrunner/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe ./CoAP.Test/bin/Release/CoAP.Test.dll
