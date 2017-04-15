e8a82520bb012e2b9ddc44908d61a588303c1619
Jim Schaad <ietf@augustcellars.com>
2017-04-15 10:52:46 -0700

    And there were two places.

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ nuget restore $SLN
 
 xbuild /p:Configuration=$VERSION $SLN
 
-mono ./testrunner/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe ./CoAP.Test/bin/$Version/$TARGET/CoAP.Test.dll
+mono ./testrunner/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe ./CoAP.Test/bin/$VERSION/$TARGET/CoAP.Test.dll
