c2768fb77ff29271a41313b16fdcf1535f5b88e5
Jim Schaad <ietf@augustcellars.com>
2017-07-07 09:27:19 -0700

    Update version number
    Update the nuspec log file

--- CoAP.NET/Properties/AssemblyInfo.cs
+++ CoAP.NET/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Com.AugustCellars.CoAP")]
 [assembly: AssemblyDescription("A CoAP framework in C#")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SmeshLink Technology")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Com.AugustCellars.CoAP")]
 [assembly: AssemblyCopyright("Copyright © SmeshLink 2011-2015 Copyright Jim Schaad 2017")]
 [assembly: AssemblyTrademark("")]
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.5")]
-[assembly: AssemblyFileVersion("1.1.5")]
+[assembly: AssemblyVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
