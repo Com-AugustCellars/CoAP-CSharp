20b3c5b4e02c1a96df8ada71dee70520a6a5f00c
Jim Schaad <ietf@augustcellars.com>
2017-10-21 10:47:00 -0700

    Add new parameter for group sending

--- CoAP.Example/CoAP.Client/ExampleClient.cs
+++ CoAP.Example/CoAP.Client/ExampleClient.cs
@@ -286,7 +286,7 @@ namespace Com.AugustCellars.CoAP.Examples
                         else if (usage == "oscoap-group") {
                             SecurityContext ctx = SecurityContext.DeriveGroupContext(key[CoseKeyParameterKeys.Octet_k].GetByteString(), key[CBORObject.FromObject(2)].GetByteString(), key[CBORObject.FromObject("SenderID")].GetByteString(), null, null, key[CoseKeyKeys.Algorithm]);
                             foreach (CBORObject recipient in key[CBORObject.FromObject("recipients")].Values) {
-                                ctx.AddRecipient(recipient[CBORObject.FromObject("RecipID")].GetByteString());
+                                ctx.AddRecipient(recipient[CBORObject.FromObject("RecipID")].GetByteString(), new OneKey( recipient[CBORObject.FromObject("sign")]));
                             }
                             SecurityContextSet.AllContexts.Add(ctx);
                         }
