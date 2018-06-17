90c139fcda020e2892a886bd7a46a93682c65626
jimsch <ietf@augustcellars.com>
2018-06-16 18:44:05 -0700

    Fix the version number and copyright notice

--- CoAP.NET/Properties/AssemblyInfo.cs
+++ CoAP.NET/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Com.AugustCellars.CoAP")]
-[assembly: AssemblyCopyright("Copyright © SmeshLink 2011-2015 Copyright Jim Schaad 2017")]
+[assembly: AssemblyCopyright("Copyright © SmeshLink 2011-2015 Copyright Jim Schaad 2017-8")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.6")]
-[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyVersion("1.1.9")]
+[assembly: AssemblyFileVersion("1.1.9")]
