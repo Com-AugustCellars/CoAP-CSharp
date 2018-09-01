86f96fe3582c9d01273fc436660b8ea8843165b2
Jim Schaad <ietf@augustcellars.com>
2018-09-01 09:43:12 -0700

    Change version of nunit runners again

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ mono nuget.exe restore $SLN
 
 msbuild /p:Configuration=$VERSION $SLN
 
-mono ./testrunner/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe ./CoAP.Test/bin/$VERSION/$TARGET/CoAP.Test.dll
+mono ./testrunner/NUnit.ConsoleRunner.3.8.0/tools/nunit3-console.exe ./CoAP.Test/bin/$VERSION/$TARGET/CoAP.Test.dll
